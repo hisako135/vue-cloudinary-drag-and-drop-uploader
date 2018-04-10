6facd34f75abcb214617b1891c2b8cedf17663b8
shirasaki-h <shirasaki-h@gnavi.co.jp>
2018-04-10 10:59:56 +0900

    modified config

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-cloudinary-drag-and-drop-uploader/',
 
     /**
      * Source Maps
